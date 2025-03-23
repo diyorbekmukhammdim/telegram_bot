f62b52ebb2d1428aee3bd8c409f3d579e43abcbb
diyorbekmuhammadiev <diyorbek.programmer@gmail.com>
2025-03-23 23:37:20 +0500

    success

--- bot.py
+++ bot.py
@@ -25,8 +25,22 @@ async def start_command(message: types.Message):
 @dp.message(lambda message: message.text == "Hayoti va ijodi")
 async def send_biography(message: types.Message):
     text = (
-        "📖 G‘afur G‘ulom (1903-1966) - O‘zbek adabiyotining yirik vakillaridan biri. "
-        "Uning mashhur asarlari orasida 'Shum bola', 'Netay' kabi asarlar bor."
+        '''📖 G‘afur G‘ulom — O‘zbekistonning taniqli yozuvchisi. Urushdan keyingi davrdagi o‘zbek adabiyoti rivojida beqiyos o‘rin tutgan shaxs.
+G‘afur G‘ulom 1903-yilning 10-may kuni Toshkentda, dehqonlar oilasida tug‘ilgan. Uning otasi savodxon bo‘lgan. U o‘zbek va tojik mumtoz adabiyotini o‘qigan, rus tilini bilgan, o‘zi ham she’rlar yozgan. Uning uyiga Muqimiy, Furqat, Asiriy, Xislat va boshqa shoirlar kelib turgan.
+1916 yilning kuzida G‘afur o‘qishga kiradi. Ota-onasining vafotidan so‘ng u ishlashga majbur bo‘lgan. Ko‘plab kasblarda o‘zini sinab ko‘rgach, u nihoyat, matbaaga harf teruvchi bo‘lib ishga kiradi, so‘ngra, pedagogik kurslarda tahsil oladi. 1919-yildan 1927-yilgacha u o‘qituvchi, maktab direktori, Ma’naviyat uyushmasi ishchilari raisi bo‘lib ishlaydi, bolalar uyini tashkil etishda faol ishtirok etadi.
+
+Adabiyotdagi faoliyati
+1923 yildan G‘afur G‘ulomning adabiy faoliyati boshlanadi. She’rlar, dostonlar, ocherklar, hajviy hikoyalar va qissalari gazeta va jurnallarda chop etila boshlaydi. 1923-yil yozilgan “Feliks farzandlari” she’rida yetim bolalar haqida gapirarkan, unda yozuvchi o‘z hayotini ifodalaydi, “Maorif va o‘qituvchi” oynomasida esa “Go‘zallik qayerda” nomli ikkinchi she’ri nashr qilinadi. Birin-ketin she’riy to‘plamlari bosmadan chiqadi: “Dinamo”, “Xitoy suratlari”, “Biz sizlar bilan tirikmiz”, “Jonli qo‘shiqlar”, “Sizga”, “Sovg‘a”, “Tong qo‘shig‘i”, “Qo‘qon” dostoni va boshqalar.
+G‘afur G‘ulomning 30-yil boshlarida yozilgan she’rlarida yangi shakllarga burilish seziladi, bunga mumtoz rus tilini o‘rganishi ham muhim darajada ta’sir ko‘rsatgan. Bundan tashqari, sanoatning o‘sishi, Turksib temir yo‘l magistralining qurilishi kabi uning ona yurtida sodir bo‘layotgan ajoyib o‘zgarishlarni ta’riflash uchun yangi lug‘at boyligi, yangi she’riy bo‘yoqlar, yangi ohang va vazn talab etilgan.
+“Dinamo” (1931), “Tirik qo‘shiqlar” (1932) — yosh shoirning yo‘nalishi yorqin namoyon bo‘lgan birinchi she’riy to‘plamlari.
+Shoirning boqiy hayot, mangu ko‘k daraxt haqidagi “Qish va qor” (1929), “Non” (1931), “Toshkent” (1933), “Qutbda saylovlar” (1937), “Men - Yahudiyman” (1941), “Qish” (1941), “Xotin” (1942), “Afsuski, afsusni qo‘shib ko‘mmadi” (1945), “Bog‘” (1934), “Qayg‘u” (1942), “Kuz keldi” (1945), “Kuzgi ko‘chatlar” (1948) kabi she’rlarida umuminsoniylik, insonparvarlik mavzulari o‘z aksini topdi.
+Ko‘pgina she’rlarida sharq donishmandi – ota timsoli mavjud: “Sen yetim emassan” (1942), “Qayg‘u” (1942), “Biri biriga shogird, biri biriga ustod” (1950), “Sizlarga - yoshlar” (1947), “Bahor taronalari” (1948) va boshqalar.
+“Netay” (1930), “Yodgor” (1936), “Shum bola” (1936-1962) qissalari va “Shariat nayranglari” (1930), “Mening o‘g‘rigina bolam” (1965) hikoyalarida chinakam xalq qahramonlari, milliyligimiz tavsirlangan.
+O‘zbekiston Fanlar akademiyasi akademiki G‘afur G‘ulom “Navoiy va bizning davr” (1948), “Folklordan o‘rganaylik” (1939) tadqiqotlarini, “Jaloliddin dramasi haqida” (1945), “Muqimiy” (1941) maqolalarini yaratgan.
+G‘afur G‘ulom qisqa, o‘tkir sujetli hikoyalar ustasi sifatida ham taniqli bo‘lib, hikoya uslubi o‘rnida u yozuvchining savol-javoblari bilan to‘ldirilgan jonli do‘stona bahs-munozara shaklida, mualliflik nutqi va kitobxonga erkin yuzlanish orqali foydalanadi. G‘afur G‘ulom tomonidan 30-yillarda yaratilgan ko‘plab nasriy asarlar yangi insoniy munosabatlarga bag‘ishlangan. U asarlarida yoritgan asosiy muammo va yechimlar – bu insonning axloqiy tarbiyasi, uning ma’naviy va madaniy rivoji sari kurashdir.
+
+G‘afur G‘ulom ko‘plab asarlarini bolalarga bag‘ishlagan. “Shum bola” hikoyasi boshqalariga qaraganda anchagina omadli hisoblanadi.
+Shuningdek, yozuvchi bolalar va o‘smirlarga bag‘ishlab, “Ikki bolalik”, “Bilaman”, “Seni Vatan kutmoqda” kabi she’rlarni yozgan.'''
     )
     await message.answer(text)
 
